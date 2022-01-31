eac42f91617bff291d187e35ecc1403f509d6908
Santiago Barbat <santib@comtura.ai>
2022-01-31 00:03:17 -0300

    Unique expiration

--- fastapi_redis/redis_client.py
+++ fastapi_redis/redis_client.py
@@ -5,27 +5,28 @@ from aioredis.client import Redis, KeyT, ExpiryT
 from aioredis.connection import EncodableT
 
 from starlette.background import BackgroundTasks
+from pydantic.main import BaseModel
 
 
 class RedisClient(Redis):
-
-    async def get(self, name: KeyT) -> Any:
+    async def get(self, name: KeyT, model: Type[BaseModel] = None) -> Any:
         """
         Gets a value with key 'name', None if not found
 
         :param name: key name
+        :param model: model to parse the result to
         :return: saved value
         """
         response = await super().get(name=name)
         if response:
-            return json.loads(response)
+            data = json.loads(response)
+            return model(**data) if data and model else data
 
     async def set(
             self,
             name: KeyT,
-            value: EncodableT,
-            expiration_seconds: Optional[ExpiryT] = None,
-            expiration_milliseconds: Optional[ExpiryT] = None,
+            value: Union[EncodableT, Type[BaseModel]],
+            expiration: timedelta = None,
             nx: bool = False,
             xx: bool = False,
             keepttl: bool = False,
@@ -35,18 +36,18 @@ class RedisClient(Redis):
 
         :param name:
         :param value:
-        :param expiration_seconds:
-        :param expiration_milliseconds:
+        :param expiration:
         :param nx:
         :param xx:
         :param keepttl:
         :return:
         """
+        if issubclass(type(value), BaseModel):
+            value = value.dict()
         value = json.dumps(value)
         return await super().set(name=name,
                                  value=value,
-                                 ex=expiration_seconds,
-                                 px=expiration_milliseconds,
+                                 ex=expiration.seconds if expiration else None,
                                  nx=nx,
                                  xx=xx,
                                  keepttl=keepttl)
@@ -55,23 +56,20 @@ class RedisClient(Redis):
                           background_tasks: BackgroundTasks,
                           name: KeyT,
                           value: EncodableT,
-                          expiration_seconds: Optional[ExpiryT] = None,
-                          expiration_milliseconds: Optional[ExpiryT] = None):
+                          expiration: timedelta = None):
         """
         Set a value using the fastapi background tasks.
 
         :param background_tasks:
         :param name:
         :param value:
-        :param expiration_seconds:
-        :param expiration_milliseconds:
+        :param expiration:
         :return:
         """
         background_tasks.add_task(self.set,
                                   name=name,
                                   value=value,
-                                  expiration_seconds=expiration_seconds,
-                                  expiration_milliseconds=expiration_milliseconds)
+                                  expiration=expiration)
 
 
 host = os.getenv('REDIS_HOST', 'localhost')
---
+++ fastapi_redis/redis_test.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+from time import sleep
+
+import pytest
+from pydantic import BaseModel
+
+from comtura.redis import redis_client
+
+
+@pytest.mark.asyncio
+async def test_redis_set():
+    await redis_client.set('object', {'redis': 'test'})
+    await redis_client.set('string', 'this is a redis test')
+    await redis_client.set('number', 100)
+    await redis_client.set('list', [1, 2, '3'])
+
+
+@pytest.mark.asyncio
+async def test_redis_get():
+    assert await redis_client.get('object') == {'redis': 'test'}
+    assert await redis_client.get('string') == 'this is a redis test'
+    assert await redis_client.get('number') == 100
+    assert await redis_client.get('list') == [1, 2, '3']
+
+
+class RedisTestModel(BaseModel):
+    value: str
+@pytest.mark.asyncio
+async def test_redis_get_model():
+    await redis_client.set('model', RedisTestModel(value='test'))
+
+    model = await redis_client.get('model', model=RedisTestModel)
+    assert type(model) == RedisTestModel
+    assert model.value == 'test'
+
+
+@pytest.mark.asyncio
+async def test_redis_remove():
+    await redis_client.delete('number')
+    assert await redis_client.get('number') == None
+
+
+@pytest.mark.asyncio
+async def test_redis_expiration():
+    await redis_client.set('number', 100, expiration=timedelta(minutes=1/60))
+    assert await redis_client.get('number') == 100
+
+    sleep(2)
+    assert await redis_client.get('number') == None
